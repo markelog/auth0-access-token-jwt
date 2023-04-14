f0afd18be9f6dc4f316ddfc6d59023105483556f
Colin Broderick <colin@cbroderick.me>
2023-04-14 19:13:06 +0200

    feat: remove dependency any/body

--- src/oauth2-bearer/get-token.test.ts
+++ src/oauth2-bearer/get-token.test.ts
@@ -1,11 +1,8 @@
 import { createServer, IncomingMessage, Server, ServerResponse } from 'http';
 import { URL } from 'url';
 import { AddressInfo } from 'net';
-// import anyBody = require('body/any');
-import {default as anyBody} from 'body/any'
 import got from 'got';
-// import typeis = require('type-is');
-import {default as typeis} from 'type-is'
+import * as typeis from 'type-is'
 import getToken from './get-token';
 
 const start = (server: Server): Promise<string> =>
@@ -15,31 +12,59 @@ const start = (server: Server): Promise<string> =>
     )
   );
 
-const handler = (req: IncomingMessage, res: ServerResponse) => {
-  let query: Record<string, string>;
+const JSON_TYPE = "application/json"
+const FORM_TYPE = "application/x-www-form-urlencoded"
+
+const parseBody = (body: string, type?: string): Record<string, string> => {
+  if (type && type.indexOf(JSON_TYPE) !== -1) {
+    return body.length > 1 ? JSON.parse(body) : JSON.parse('{}')
+  }
+
+  if (type && type.indexOf(FORM_TYPE) !== -1) {
+    const parsedData = new URLSearchParams(body);
+    const dataObj = {};
+    for (const pair of parsedData.entries()) {
+      dataObj[pair[0]] = pair[1];
+    }
+    return dataObj
+  }
+  return {}
+}
+
+const handler =  (req: IncomingMessage, res: ServerResponse) => {
+  let query: Record<string, string> = {};
+  
   new URL(req.url as string, 'http://localhost').searchParams.forEach(
     (val, key) => {
       query = query || {};
       query[key] = val;
     }
   );
-  anyBody(req, res, (err, body) => {
-    try {
-      res.end(
-        getToken(
-          req.headers,
-          query,
-          body as Record<string, string>,
-          !!typeis.is(req.headers['content-type'] as string, ['urlencoded'])
-        )
-      );
-    } catch (e) {
-      res.statusCode = e.statusCode;
-      res.statusMessage = e.message;
-      res.end();
-    }
+  
+  const chunks: Buffer[] = [];
+
+  req.on("data", (chunk) => {
+    chunks.push(chunk);
   });
-};
+
+  req.on("end", () => {
+    const data = Buffer.concat(chunks);
+    const dataString =  data.toString()
+    const parsedBody = parseBody(dataString, req.headers['content-type'])
+
+    try {
+      const token = getToken(req.headers, query, parsedBody, !!typeis.is(req.headers['content-type'] as string, ['urlencoded']))
+      
+      return res.end(token);
+    } catch (error) {
+
+      res.statusCode = error.statusCode;
+      res.statusMessage = error.message;
+    }
+
+    return res.end();
+  });
+}
 
 describe('get-token', () => {
   let server: Server;
